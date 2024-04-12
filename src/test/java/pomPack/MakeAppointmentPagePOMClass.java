e27338fc606720b994515f1f449130d02974827b
VivShimpi <viv.Shimpi09@gmail.com>
2024-04-12 14:38:32 +0530

    booking confirmation code add

---
+++ src/test/java/pomPack/MakeAppointmentPagePOMClass.java
@@ -0,0 +1,114 @@
+package pomPack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class MakeAppointmentPagePOMClass 
+{
+
+WebDriver driver;
+Select sel;
+
+
+
+@FindBy(xpath="//select[@id='combo_facility']")WebElement facilityDropDown;
+public void clickfacilityDropDown()
+{
+	facilityDropDown.click();
+	sel.selectByVisibleText("Hongkong CURA Healthcare Center");
+	
+}
+
+@FindBy(xpath="//input[@id='chk_hospotal_readmission']")WebElement checkbox ;
+public void clickCheckBox()
+{
+	checkbox.click();
+}
+
+@FindBy(xpath="//input[@id='radio_program_medicaid']")WebElement radioMedicaidBtn ;
+public void clickRadioMedicaidBtn()
+{
+	radioMedicaidBtn.click();
+}
+
+
+
+//***************Date picker code ****************
+
+
+
+@FindBy(xpath="//input[@id='txt_visit_date']")WebElement datePicker ;
+public void clickDatePicker()
+{
+	
+	datePicker.click();
+	
+}
+
+@FindBy(xpath="(//th[@class='datepicker-switch'])[1]")WebElement  monthYear;
+public String gettextfromMonthYEar()
+{
+	return monthYear.getText();
+}
+
+@FindBy(xpath="(//th[@class='next'])[1]")WebElement  nextBtbOnDatePicker;
+public void clickNextBtbOnDatePicker()
+{
+	nextBtbOnDatePicker.click();;
+}
+
+@FindBy(xpath="//td[@class='day']")WebElement  gettextDayOnDatePicker;
+public String getTextDay()
+{
+	return gettextDayOnDatePicker.getText();
+}
+
+
+
+public void selectday(String day)
+{
+	WebElement  DayOnDatePicker = driver.findElement(By.xpath
+     ("//table[@class='table-condensed']//td[contains(text(),"+day+")]"));
+	DayOnDatePicker.click();
+	
+}
+@FindBy(xpath="//td[@class='active day']")WebElement selectedday;
+public String getClassDay()
+{
+	return selectedday.getText();
+	
+}
+
+//************************************************************************
+
+@FindBy(xpath="//textarea[@id='txt_comment']")WebElement commentBox;
+public void sendkeysToCommentBox(String comment)
+{
+	commentBox.sendKeys(comment);
+
+}
+
+@FindBy(xpath="//button[@id='btn-book-appointment']")WebElement bookAppointmentBtn;
+public void clickOnbookAppointmentBtn()
+{
+	bookAppointmentBtn.click();
+}
+
+@FindBy(xpath="//h2[text()='Appointment Confirmation']")WebElement confirmationText;
+public String getConfirmationText()
+{
+	return confirmationText.getText();
+}
+
+
+public MakeAppointmentPagePOMClass(WebDriver driver)
+{  
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+	sel = new Select(facilityDropDown);
+}
+}
---
+++ src/test/java/testPack/TC02_verifyMakeAppointmentFunctionality.java
@@ -0,0 +1,137 @@
+package testPack;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pomPack.LoginPagePOMClass;
+import pomPack.MakeAppointmentPagePOMClass;
+import utilitiesPack.UtilityClass;
+
+public class TC02_verifyMakeAppointmentFunctionality extends TestBaseClass
+{
+	String monthYear = "May 2024";
+	String day = "13";
+	String comment = "hi, my name is vivek and I want an appointment";
+	@Test
+	public void verifyMakeAppointmentFunctionality() throws InterruptedException, IOException
+	{
+		LoginPagePOMClass lp = new LoginPagePOMClass(driver);
+		lp.clickMakeAppointmentBtn();	
+		log.info("Clicked on Make Appointment button");
+		Thread.sleep(2000);
+		
+		UtilityClass.screenShotMethod(driver, "loginPage");
+		log.info("login page snap taken");
+		
+		
+		lp.sendUserName();
+		log.info("username is entered");
+		Thread.sleep(2000);
+
+		
+		lp.sendPassword();
+		log.info("password is entered");
+		Thread.sleep(2000);
+
+		lp.clickLoginBtn();
+		log.info("clicked ligin btn");
+		Thread.sleep(2000);
+		
+		UtilityClass.screenShotMethod(driver, "MakeAppointmentPage");
+		log.info("MakeAppointmentpage snap taken");
+		
+		log.info("Went on Make Appointment Page");
+		
+		MakeAppointmentPagePOMClass map = new MakeAppointmentPagePOMClass(driver);
+		map.clickfacilityDropDown();
+		log.info("salect from dropdown done");
+		Thread.sleep(2000);
+		
+		map.clickCheckBox();
+		log.info("box is checked");
+		Thread.sleep(2000);
+		
+		map.clickRadioMedicaidBtn();
+		log.info("clicked on radio button");
+		Thread.sleep(2000);
+		
+//		***************Date picker code ****************
+		map.clickDatePicker();
+		log.info("datepicker is selected");
+		Thread.sleep(2000);
+		
+        
+       //loop for select  month and year		
+		while(true)
+		{
+			String text = map.gettextfromMonthYEar();
+			if(text.equals(monthYear))
+			{
+				break;
+			}
+			else
+			{
+			map.clickNextBtbOnDatePicker();
+			Thread.sleep(2000);
+            }
+		}
+		
+		map.selectday(day);
+		Thread.sleep(2000);
+		log.info("select day of APPOINTMENT");
+		
+		
+		log.info("validate day of appointment");
+		String expectedday = "13";
+		String actualday = map.getClassDay();
+		System.out.println(map.getClassDay());
+		
+		Assert.assertEquals(actualday, expectedday, actualday);
+		
+		if(expectedday.equals(actualday))
+		{
+			log.info("Case passed");
+		}
+		else
+		{
+			log.info("Case failed");
+	    }
+		
+		
+		map.sendkeysToCommentBox(comment);
+		Thread.sleep(2000);
+		log.info("comment sent to box");
+		
+		
+		
+		map.clickOnbookAppointmentBtn();
+		Thread.sleep(2000);
+		log.info("Clicked on book appointment button");
+		
+		
+		UtilityClass.screenShotMethod(driver, "AppointmentConfirmation");
+		log.info("AppointmentConfirmation snap taken");
+		
+		
+		log.info("apply verification for booking confirmation");
+		
+		String expectedmsg = "Appointment Confirmation";
+		String actualmsg = map.getConfirmationText();
+		
+		
+		Assert.assertEquals(actualmsg, expectedmsg, actualmsg);
+		if(expectedmsg.equals(actualmsg))
+		{
+			log.info("case passed");
+		}
+		else
+		{
+			log.info("case fail");
+		}
+		
+		
+		
+	}
+}
